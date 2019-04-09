3abb5aa566f89af1706d64517a69a3425443ef28
kentaroy47 <kentaroy@stanford.edu>
2019-04-09 14:35:29 -0400

    change batch for gpu=2

--- train_cifar10.py
+++ train_cifar10.py
@@ -44,7 +44,7 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True, num_workers=8)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
